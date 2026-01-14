8fc70de897853a653ca75fbd897c570f993575b8
Rock002 <chaykhoyan@mail.ru>
2026-01-14 16:37:15 +0300

    Initial structure

---
+++ src/main/java/com/example/MeowDate/MeowDateApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeowDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowDateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MeowDate/MeowDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MeowDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
